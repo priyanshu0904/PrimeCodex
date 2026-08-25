94e2b5c3ad16f202b4150f7711a5ff454507fc7f
Priyanshu Raj <143980202+priyanshu0904@users.noreply.github.com>
2026-08-25 17:56:14 +0530

    Update MultiplicationTableUsingFor.java

--- Coding/Java Development/Java/Source Code/04_Control_Statements/Practice_Exersices/src/MultiplicationTableUsingFor.java
+++ Coding/Java Development/Java/Source Code/04_Control_Statements/Practice_Exersices/src/MultiplicationTableUsingFor.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class MultiplicationTableUsingFor {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Welcome to Multiplication Table....");
+        System.out.println("Welcome to Multiplication Table Generator....");
         System.out.print("Enter the number you want to print table of: ");
         int num = sc.nextInt();
 
