c954a627f6e2655bfc6bf5ddbe5900af4a729c4a
Priyanshu <priyanshuraj0904@gmail.com>
2026-09-13 08:32:01 +0530

    Problem practiced

--- Coding/Java Development/Java/Source Code/Practice_Problems/Solutions/src/Problem6/TypeConversion.java
+++ Coding/Java Development/Java/Source Code/Practice_Problems/Solutions/src/Problem6/TypeConversion.java
@@ -7,9 +7,44 @@ public class TypeConversion {
         int i = 128964;
         long l = 1200000L;
         float f = 120.598f;
-        double d = 120.568978;
+        double d = 120.56897889988;
         char c = 'c';
+        System.out.println("b: " + b);
+        System.out.println("s: " + s);
+        System.out.println("i: " + i);
+        System.out.println("l: " + l);
+        System.out.println("f: " + f);
+        System.out.println("d: " + d);
+        System.out.println("c: " + c);
+        System.out.println();
 
+        //Widening
+        double d1 = f;
+        float f1 = l;
+        long l1 = i;
+        int i1 = s;
+        short s1 = b;
+        int ix = c;
+        System.out.println("s1: " + s1);
+        System.out.println("i1: " + i1);
+        System.out.println("l1: " + l1);
+        System.out.println("f1: " + f1);
+        System.out.println("d1: " + d1);
+        System.out.println("ix: " + ix);
+        System.out.println();
 
+        //Narrowing
+        byte b2 = (byte) s;
+        short s2 = (short) i;
+        int i2 = (int) l;
+        long l2 = (long) f;
+        float f2 = (float) d;
+        char c2 = (char) b;
+        System.out.println("b2: " + b2);
+        System.out.println("s2: " + s2);
+        System.out.println("i2: " + i2);
+        System.out.println("l2: " + l2);
+        System.out.println("f2: " + f2);
+        System.out.println("c2: " + c2);
     }
 }
---
+++ Coding/Java Development/Java/Source Code/Practice_Problems/Solutions/src/Problem7/EscapeSequence.java
@@ -0,0 +1,7 @@
+package Problem7;
+
+public class EscapeSequence {
+    public static void main(String[] args) {
+        System.out.println("Hello tabs\t\tThis is my backslash\\ \t \"Hello\"... \nAdd this to new line");
+    }
+}
