58315f3690fcbe146aea89b7a286dfa91757369f
Priyanshu <priyanshuraj0904@gmail.com>
2026-09-13 08:33:29 +0530

    Problem practiced

--- Coding/Java Development/Java/Source Code/Practice_Problems/Solutions/src/Problem7/EscapeSequence.java
+++ Coding/Java Development/Java/Source Code/Practice_Problems/Solutions/src/Problem7/EscapeSequence.java
@@ -2,6 +2,6 @@ package Problem7;
 
 public class EscapeSequence {
     public static void main(String[] args) {
-        System.out.println("Hello tabs\t\tThis is my backslash\\ \t \"Hello\"... \nAdd this to new line");
+        System.out.println("Hello tabs\t\tThis is my backslash\\ \t \"Hello\"... \nAdd this to new line\nAdd this to another new line");
     }
 }
